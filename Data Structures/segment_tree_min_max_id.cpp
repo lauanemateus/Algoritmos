06c67502db62ce5e6e97e633d5bd71471f6cd75c
lauanemateus <88512406+lauanemateus@users.noreply.github.com>
2024-11-19 16:20:41 -0300

    Update segment_tree_min_max_id.cpp

--- Data Structures/segment_tree_min_max_id.cpp
+++ Data Structures/segment_tree_min_max_id.cpp
@@ -15,7 +15,6 @@ struct no{
 int teste;
 int n;
 no t[4*N], vetor[N], nulo;
-int nulo_min = n, nulo_max = n+1;
 
 no merge(no esq, no dir){
     no ans;
@@ -34,38 +33,38 @@ no merge(no esq, no dir){
     return ans;
 }
 
-void build(int v, int tl, int tr) {
+void build(no vetor[], no t[], int v, int tl, int tr) {
     if (tl == tr) {
         t[v]=vetor[tl];
     } else {
         int tm = (tl + tr) / 2;
-        build(v*2, tl, tm);
-        build(v*2+1, tm+1, tr);
+        build(vetor, t, v*2, tl, tm);
+        build(vetor, t, v*2+1, tm+1, tr);
         t[v] = merge(t[v*2], t[v*2+1]);
     }
 }
 
-no query(int v, int tl, int tr, int l, int r) {
+no query(no vetor, no t[], int v, int tl, int tr, int l, int r) {
     if (l > r) 
         return nulo;
     if (l == tl && r == tr) {
         return t[v];
     }
     int tm = (tl + tr) / 2;
-    return merge(query(v*2, tl, tm, l, min(r, tm))
-           , query(v*2+1, tm+1, tr, max(l, tm+1), r));
+    return merge(query(vetor, t, v*2, tl, tm, l, min(r, tm))
+           , query(vetor, t, v*2+1, tm+1, tr, max(l, tm+1), r));
 }
 
 // tl e tr se referem à árvore
-void update(int v, int tl, int tr, int pos, int new_val) {
+void update(no vetor, no t[], int v, int tl, int tr, int pos, int new_val) {
     if (tl == tr) {
         t[v].val_min = t[v].val_max = new_val;
     } else {
         int tm = (tl + tr) / 2;
         if (pos <= tm)
-            update(v*2, tl, tm, pos, new_val);
+            update(vetor, t, v*2, tl, tm, pos, new_val);
         else
-            update(v*2+1, tm+1, tr, pos, new_val);
+            update(vetor, t, v*2+1, tm+1, tr, pos, new_val);
         t[v] = merge(t[v*2], t[v*2+1]);
     }
 }
@@ -79,22 +78,15 @@ void solve(){
         vetor[i].val_max = vetor[i].val_min = x;
         vetor[i].id_min = vetor[i].id_max = i;
     }
-    build(1, 0, n-1);
+    build(vetor, t, 1, 0, n-1);
 
-    int q;
-    cin>>q;
-    while(q--){
-        int l, r;
-        cin>>l>>r;
-        no ans = query(1, 0, n-1, l, r);
-        cout<<ans.val_max<<" "<<ans.val_min<<"\n";
-    }
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    solve();
+    cin>>teste;
+    while(teste--) solve();
     return 0;
 }
